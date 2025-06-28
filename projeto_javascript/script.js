8622bb9aa9a32208379920e238f14f9a8853d18d
Rafael Moreira Rosa <156961270+FaelMoreiraRosa@users.noreply.github.com>
2025-06-28 01:51:48 -0300

    Delete projeto_javascript directory

--- projeto_javascript/script.js
+++
@@ -1,9 +0,0 @@
-function abrirExemplo1() {
-  window.location.href = 'Exemplo1/index.html';
-}
-function abrirExemplo2() {
-  window.location.href = 'Exemplo2/index.html';
-}
-function abrirExemplo3() {
-  window.location.href = 'Exemplo3/index.html';
-}
